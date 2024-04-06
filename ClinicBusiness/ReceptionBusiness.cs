970bac19ea4386d4f436c9fd3b72aa125437f5e2
dev-sina <haghanisina3989@gmail.com>
2024-04-06 18:55:01 +0330

    add the patient's reception page + modify mainlayout

---
+++ ClinicBusiness/ReceptionBusiness.cs
@@ -0,0 +1,21 @@
+﻿using ClinicData;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicBusiness
+{
+   
+    public class ReceptionBusiness
+    {
+        ClReception obj = new ClReception();
+        public byte InsertReception(string firstname, string lastname, int age, string beneficiarycode)
+        {
+            return obj.InsertReception(firstname, lastname, age, beneficiarycode);
+        }
+
+    }
+}
---
+++ ClinicData/ClReception.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicData
+{
+    public class ClReception
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-HOVKQKU; Initial Catalog=BlazorProject_DB;Integrated Security=true;TrustServerCertificate=True;MultipleActiveResultSets=true;User Id=sa;Password=sina.hgh0832;");
+        SqlCommand cmd;
+        public byte InsertReception(string firstname, string lastname, int age, string beneficiarycode)
+        {
+            con.Open();
+            cmd = new SqlCommand("insert into ReceptionCS values(N'" + firstname + "',N'" + lastname + "',N'" + age + "',N'" + beneficiarycode + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return 0;
+        }
+
+    }
+}
